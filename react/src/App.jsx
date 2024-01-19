4ce8a22f9acff86a14cf22efa24cf5e1389c8101
Felix Mburu <felixmburumwangi@gmail.com>
2024-01-19 23:21:44 +0300

    Solving cors connection problems and implementing a simple chat room to demo the socket.io working

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,99 +1,48 @@
-import  { useEffect } from 'react';
-import io from 'socket.io-client';
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client'
 
 const socket = io('http://localhost:4000');
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [inputMessage, setInputMessage] = useState('');
+  
+
   useEffect(() => {
-
-    
-    socket.connect('connect', () => {
-      console.log('Connected to server');
-    });
-
-    socket.on('disconnect', () => {
-      console.log('Disconnected from server');
+    socket.on('chatMessage', (message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
     });
 
     return () => {
       socket.disconnect();
     };
-
- 
   }, []);
 
+  const sendMessage = () => {
+    socket.emit('chatMessage', inputMessage);
+    setInputMessage('');
+  };
 
   return (
     <div className="App">
-      <h1>Socket.io </h1>
+      <h1>Socket.io Chat Room</h1>
+      <div>
+        <ul>
+          {messages.map((msg, index) => (
+            <li key={index}>{msg}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <input
+          type="text"
+          value={inputMessage}
+          onChange={(e) => setInputMessage(e.target.value)}
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { useEffect } from 'react';
-// import io from 'socket.io-client';
-
-// const socket = io('http://localhost:3000');
-
-// function App() {
-//   useEffect(() => {
-//     socket.on('connect', () => {
-//       alert('Connected to server');
-//     });
-
-//     socket.on('disconnect', () => {
-//       console.log('Disconnected from server');
-//     });
-
-//     return () => {
-//       socket.disconnect();
-//     };
-//   }, []);
-
-//   return (
-//     <div className="App">
-//       <h1>Socket.io Example</h1>
-//     </div>
-//   );
-// }
-
-// export default App;
-
--- server/app.js
+++ server/app.js
@@ -5,74 +5,35 @@ const cors = require('cors');
 
 const app = express();
 const corsOptions = {
-      origin: 'http://localhost:5173',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true,
-      optionsSuccessStatus: 204,
-    };
-app.use(cors(corsOptions))
+  origin: 'http://localhost:5173',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
 
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
+});
 
 io.on('connection', (socket) => {
   console.log('A user connected');
 
+  socket.on('chatMessage', (message) => {
+    console.log('Received message:', message);
 
+    io.emit('chatMessage', message);
+  });
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
 });
 
-const port = process.env.PORT || 4000;
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+server.listen(4000, () => {
+  console.log('Server is running on port 4000');
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require('express')
-// const http = require('http')
-// const socketIo = require('socket.io')
-// const cors = require('cors')
-// ///
-// const app = express()
-// app.use(cors())
-// const server = http.createServer(app)
-// const io =socketIo(server, {
-//     cors:{
-//         origin: 'http://localhost:5173/',
-//     },
-// })
-// ///
-// io.on('connection', (socket)=>{
-//     console.log("USER CONNECTED")
-// })
-// server.listen(3000, ()=>{
-//     console.log("SERVER RUNING")
-// })
